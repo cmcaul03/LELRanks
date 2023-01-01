99c47c11a1e697c564d2ea99764eac54e2db18bd
cmcaul03 <cameron.mcauliffe7@gmail.com>
2023-01-02 09:54:41 +1100

    Autoupdate

--- LELRankings.ps1
+++ LELRankings.ps1
@@ -1,8 +1,22 @@
 ﻿$tourneys = Invoke-WebRequest -Headers @{"Cache-Control"="no-cache"} "https://raw.githubusercontent.com/cmcaul03/LELRanks/main/current-tourney?raw=true"
 $tourneys = $tourneys.content
-$csv_path = "D:\AOERanks\"
-$csv_name = "AOE4World Dump 03-12-2022.csv"
-$csv = import-csv $csv_path$csv_name
+$download_name = "AOE4World_Dump"
+$current_path = split-path -parent $MyInvocation.MyCommand.Definition
+
+$FolderName = "$current_path\web\"
+if (Test-Path $FolderName) {
+}
+else
+{
+    New-Item $FolderName -ItemType Directory
+}
+
+
+$dumps = Invoke-RestMethod https://aoe4world.com/api/v0/data_dumps
+$dump = $dumps | Where-Object {$_.key -eq "leadersboards/rm_1v1/elo"}
+$gz = Invoke-RestMethod $dump.url -OutFile "$current_path$download_name.gz"
+C:\'Program Files'\7-Zip\7z.exe x "$current_path$download_name.gz" -y
+$csv = import-csv "$current_path\rm_1v1_0.csv"
 
     Foreach ($tourney in $tourneys -split "`n") {
 
@@ -231,8 +245,8 @@ $csv = import-csv $csv_path$csv_name
         }elseif ($player -eq "Trakor") {
             $player = "PSR.Trakor"
             $profileId = "1712108"
-        }elseif ($player -eq "Shuuki") {
-            $player = "Shineko"
+        }elseif ($player -eq "Shineko") {
+            $player = "[NW] NakanoMikuSimp❤"
             $profileId = "10400991"
         }elseif ($player -eq "ingsok") {
             $player = "ingsoc8106"
@@ -552,20 +566,16 @@ Add-Type -AssemblyName System.Web
 
         if ($tourney -like "*rising-empires*") {
                 $lel_html = $lel_team_data | Sort-Object -Property "Hidden Rank", "Bracket" | ConvertTo-Html "AOE Name","Start GG Name","Ladder Elo","Ladder Rank","Hidden Elo","Hidden Rank","Games Played","Registered For", "Bracket" -Head ($head + " There were $lel_total_players players found. </p>")
-                [System.Web.HttpUtility]::HtmlDecode($lel_html) |  Out-File "D:\AOERanks\web\LEL-$tourney.html"
+                [System.Web.HttpUtility]::HtmlDecode($lel_html) |  Out-File "D:\GIT\LELRanks\web\LEL-$tourney.html"
 
                 $twc_html = $twc_team_data | Sort-Object -Property "Registered For", "Hidden Rank", "Bracket" | ConvertTo-Html "AOE Name","Start GG Name","Ladder Elo","Ladder Rank","Hidden Elo","Hidden Rank","Games Played","Registered For", "Bracket" -Head ($head + " There were $twc_total_players players found. </p>")
-                [System.Web.HttpUtility]::HtmlDecode($twc_html) |  Out-File "D:\AOERanks\web\TWC-$tourney.html"
+                [System.Web.HttpUtility]::HtmlDecode($twc_html) |  Out-File "D:\GIT\LELRanks\web\TWC-$tourney.html"
         } else {
                 $other_html = $other_team_data | Sort-Object -Property "Registered For", "Hidden Rank", "Bracket" | ConvertTo-Html "AOE Name","Start GG Name","Ladder Elo","Ladder Rank","Hidden Elo","Hidden Rank","Games Played","Registered For", "Bracket" -Head ($head + " There were $other_total_players players found. </p>")
-                [System.Web.HttpUtility]::HtmlDecode($other_html) |  Out-File "D:\AOERanks\web\OTHER-$tourney.html"
+                [System.Web.HttpUtility]::HtmlDecode($other_html) |  Out-File "D:\GIT\LELRanks\web\OTHER-$tourney.html"
         }
 
 }
 }
 
-pause
-
 powershell.exe "D:\GIT\LELRanks\GenerateMenu.ps1"
-
-pause
