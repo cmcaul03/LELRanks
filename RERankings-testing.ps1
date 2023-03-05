d8dd1d3bc953072998dc7191b63aa4ce4d0c63d3
cmcaul03 <cameron.mcauliffe7@gmail.com>
2023-03-05 13:57:45 +1100

    Autoupdate

--- RERankings-testing.ps1
+++ RERankings-testing.ps1
@@ -1,4 +1,7 @@
-﻿Function Get-TournamentDetails ($tournament_name) {
+﻿$current_path="D:\GIT\LELRanks\"
+git pull ($current_path)
+
+Function Get-TournamentDetails ($tournament_name) {
     $StartGG_URI = "https://api.start.gg/gql/alpha"
     $cred = gc "C:\StartGG_cred.txt"
     #$tournament_name = "low-elo-legends"
@@ -64,7 +67,7 @@ Function Get-Stats ($player_name) {
             write-host "https://aoe4world.com/api/v0/players/$profile_id"
             Foreach ($game_mode in $game_modes) {
                     $elo_object | Add-Member -MemberType NoteProperty -Name $game_mode -Value $response.modes.$game_mode.rating -Force
-                    $elo_object | Add-Member -MemberType NoteProperty -Name $game_mode+"_win_rate" -Value $response.modes.$game_mode.win_rate -Force
+                    $elo_object | Add-Member -MemberType NoteProperty -Name $game_mode"_win_rate" -Value $response.modes.$game_mode.win_rate -Force
                 }
             }
         else {
@@ -76,7 +79,65 @@ Function Get-Stats ($player_name) {
 }
 
 Function Export-Data ($tournament_entrants, $event) {
+
+    $Time = Get-Date
+    $Time = $Time.ToUniversalTime()
+
+    $head = @"
+    <style>
+    table {
+        border-collapse: collapse;
+        margin: 25px 0;
+        font-size: 0.9em;
+        font-family: sans-serif;
+        min-width: 400px;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+        width: 80%;
+        padding-left: 10%;
+        margin-left: 10%;
+    }
+
+    p {
+        border-collapse: collapse;
+        margin: 25px 0;
+        font-size: 0.9em;
+        font-family: sans-serif;
+        padding-left: 10%;
+    }
+
+
+    tbody tr th {
+        background-color: #009879;
+        color: #ffffff;
+        text-align: left;
+    }
+
+    th,
+    td {
+        padding: 12px 15px;
+    }
+
+    tbody tr {
+        border-bottom: 1px solid #dddddd;
+    }
+
+    tbody tr:nth-of-type(even) {
+        background-color: #f3f3f3;
+    }
+
+    tbody tr:last-of-type {
+        border-bottom: 2px solid #009879;
+    }
+    </style>
+    <p>Last updated at: $time UTC. <a href="http://risingempires.cammcauliffe.com">Shit Menu</a>
+"@
+
+    Add-Type -AssemblyName System.Web
+
     $tournament_entrants | Sort-Object -Property "Team Name" | Export-CSV  "D:\GIT\LELRanks\web\$event-testing-tourney.csv" -NoTypeInformation
+
+    $export_html = $tournament_entrants | Sort-Object -Property "Elo", "Event" | ConvertTo-Html -Head ($head + " There were "+$tournament_entrants.Length+" players found. </p>")
+    [System.Web.HttpUtility]::HtmlDecode($export_html) |  Out-File "D:\GIT\LELRanks\web\$event-testing.html"
 }
 
 Function Get-EntrantData ($tournament_name) {
@@ -87,7 +148,7 @@ Function Get-EntrantData ($tournament_name) {
         Foreach ($entrant in $event.entrants.nodes) {
             $entrant_object = New-Object PSObject
             $stats = Get-Stats $entrant.participants[0].gamerTag
-            $entrant_object | Add-Member -MemberType NoteProperty -Name "Name" -Value ("<a href=`"https://aoe4world.com/players/"+$stats.rm_solo+"/>`""+$entrant.participants[0].gamerTag+"</a>")  -Force
+            $entrant_object | Add-Member -MemberType NoteProperty -Name "Name" -Value ("<a href=`"https://aoe4world.com/players/"+$stats.ProfileID+">`""+$entrant.participants[0].gamerTag+"</a>")  -Force
             $entrant_object | Add-Member -MemberType NoteProperty -Name "Ladder Rating" -Value $stats.rm_solo -Force
             $entrant_object | Add-Member -MemberType NoteProperty -Name "Ladder Rank" -Value $stats.rm_1v1_elo -Force
             $entrant_object | Add-Member -MemberType NoteProperty -Name "Elo" -Value $stats.rm_1v1_elo -Force
@@ -95,6 +156,7 @@ Function Get-EntrantData ($tournament_name) {
             $entrant_object | Add-Member -MemberType NoteProperty -Name "Event" -Value $event.Name -Force
             $tournament_entrants += $entrant_object
         }
+        write-host $event.Name
         if ($event.Name -match "Low Elo Legends") {
             $lel_data += $tournament_entrants
         } elseif ($event.Name -match "Warchief") {
@@ -106,9 +168,11 @@ Function Get-EntrantData ($tournament_name) {
 
     if ($lel_data -ne $null) {
         Export-Data $lel_data ("LEL-$tournament_name")
-    } elseif ($twc_data -ne $null) {
+    }
+    if ($twc_data -ne $null) {
         Export-Data $twc_data ("TWC-$tournament_name")
     }
 }
 
 Get-EntrantData "rising-empires-weeklies-22"
+powershell.exe "$current_path\GenerateMenu.ps1"
