a14821c1d4cf3dd3cb4e146408646ce50a197d3c
cmcaul03 <cameron.mcauliffe7@gmail.com>
2023-09-30 08:23:12 +1000

    Autoupdate

--- LELRankings.ps1
+++ LELRankings.ps1
@@ -45,6 +45,9 @@ $csv_date = $csv_date.CreationTime
 
     $players_response = Invoke-RestMethod "https://api.start.gg/tournament/$tourney`?expand[]=entrants"
 
+    $map_pool = node "D:\testing\scrape.js" $tourney
+    $map_pool | out-file "$current_path\web\map_pool.html"
+
     if ($players_response -eq $null) {
         $tourney = $tourney.Replace('empires','empire')
         $players_response = Invoke-RestMethod "https://api.start.gg/tournament/$tourney`?expand[]=entrants"
